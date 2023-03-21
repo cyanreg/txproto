cyanreg/txproto

commit 669f466b41f50be9846c30476756a687ed3b9133

build: Allow to disable interface

src/lua_generic_api.c
@@ -16,7 +16,10 @@
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
  */
 
+#ifdef HAVE_INTERFACE
 #include "interface_common.h"
+#endif
+
 #include "iosys_common.h"
 #include "encoding.h"
 #include "decoding.h"
@@ -116,8 +119,10 @@ static ctrl_fn get_ctrl_fn(void *ctx)
         return sp_demuxer_ctrl;
     case SP_TYPE_FILTER:
         return sp_filter_ctrl;
+#ifdef HAVE_INTERFACE
     case SP_TYPE_INTERFACE:
         return sp_interface_ctrl;
+#endif
     case SP_TYPE_AUDIO_SOURCE:
     case SP_TYPE_AUDIO_SINK:
     case SP_TYPE_AUDIO_BIDIR:
@@ -188,8 +193,10 @@ static AVBufferRef *sp_ctx_get_fifo(void *ctx, int out)
             return ((EncodingContext *)ctx)->dst_packets;
         else
             return ((EncodingContext *)ctx)->src_frames;
+#ifdef HAVE_INTERFACE
     case SP_TYPE_INTERFACE:
         return sp_interface_get_fifo(ctx);
+#endif
     case SP_TYPE_DECODER:
         if (out)
             return ((DecodingContext *)ctx)->dst_frames;
@@ -432,6 +439,7 @@ int sp_lua_generic_link(lua_State *L)
         dst_filt_pad = av_strdup(dst_pad_name);
         src_ctrl_fn = sp_filter_ctrl;
         dst_ctrl_fn = sp_filter_ctrl;
+#ifdef HAVE_INTERFACE
     } else if (EITHER(obj1, obj2, SP_TYPE_INTERFACE, SP_TYPE_FILTER)) {
         src_ref = PICK_REF(obj1, obj2, SP_TYPE_FILTER);
         dst_ref = PICK_REF(obj1, obj2, SP_TYPE_INTERFACE);
@@ -448,6 +456,7 @@ int sp_lua_generic_link(lua_State *L)
         dst_ref = PICK_REF(obj1, obj2, SP_TYPE_INTERFACE);
         src_ctrl_fn = ((IOSysEntry *)src_ref->data)->ctrl;
         dst_ctrl_fn = sp_interface_ctrl;
+#endif
     } else if (EITHER(obj1, obj2, SP_TYPE_ENCODER, SP_TYPE_DECODER)) {
         src_ref = PICK_REF(obj1, obj2, SP_TYPE_DECODER);
         dst_ref = PICK_REF(obj1, obj2, SP_TYPE_ENCODER);

meson.build
@@ -45,12 +45,6 @@ dependencies = [
     dependency('libavfilter', version: '>= 7.79.100'),
     dependency('libavutil', version: '>= 56.43.100'),
 
-    # libplacebo
-    dependency('libplacebo', version: '>= 3.120.0'),
-
-    # vulkan
-    dependency('vulkan', version: '>= 1.1'),
-
     # lua
     dependency('lua', version: ['>= ' + lua_ver, '< ' + lua_ver_maj.to_string() + '.' + (lua_ver_min + 1).to_string()]),
 

src/lua_api.c
@@ -29,7 +29,10 @@
 #include "encoding.h"
 #include "decoding.h"
 #include "filtering.h"
+
+#ifdef HAVE_INTERFACE
 #include "interface_common.h"
+#endif
 
 #include "lua_generic_api.h"
 #include "lua_api_utils.h"
@@ -402,6 +405,7 @@ static int lua_generic_schedule(lua_State *L)
     return 1;
 }
 
+#ifdef HAVE_INTERFACE
 static int lua_interface_create_display(lua_State *L)
 {
     TXMainContext *ctx = lua_touserdata(L, lua_upvalueindex(1));
@@ -499,6 +503,7 @@ static int lua_create_interface(lua_State *L)
 
     return 1;
 }
+#endif
 
 static int lua_create_muxer(lua_State *L)
 {
@@ -1646,7 +1651,9 @@ const struct luaL_Reg sp_lua_lib_fns[] = {
     { "create_decoder", lua_create_decoder },
     { "create_filter", lua_create_filter },
     { "create_filtergraph", lua_create_filtergraph },
+#ifdef HAVE_INTERFACE
     { "create_interface", lua_create_interface },
+#endif
 
     { "set_epoch", lua_set_epoch },
 

src/meson.build
@@ -11,10 +11,6 @@ sources = [
     # Sources
     'iosys_common.c',
 
-    # Outputs
-    'interface.c',
-    'interface_common.c',
-
     # Muxing
     'muxing.c',
 
@@ -51,6 +47,18 @@ if libedit.found()
     sources += 'cli.c'
     conf.set('HAVE_LIBEDIT', 1)
     features += ', ' + libedit.name() + ' ' + libedit.version()
+endif
+
+# libplacebo + vulkan
+libplacebo = dependency('libplacebo', version: '>= 3.120.0', required: get_option('interface'))
+vulkan = dependency('vulkan', version: '>= 1.1', required: get_option('interface'))
+have_interface = libplacebo.found() and vulkan.found()
+if have_interface
+    dependencies += libplacebo
+    dependencies += vulkan
+    sources += 'interface.c'
+    sources += 'interface_common.c'
+    conf.set('HAVE_INTERFACE', 1)
 endif
 
 # libavdevice
@@ -105,8 +113,12 @@ if have_wayland
     dependencies += wayland_deps.get(3)
     dependencies += wayland_deps.get(4)
     sources += 'iosys_wayland.c'
-    sources += 'interface_wayland.c'
     sources += 'wayland_common.c'
+
+    if have_interface
+        sources += 'interface_wayland.c'
+    endif
+
     conf.set('HAVE_WAYLAND', 1)
     features += ', ' + 'wayland' + ' ' + wayland_deps.get(0).version()
 
@@ -126,6 +138,7 @@ executable('txproto',
 summary({
     'CLI': libedit.found(),
     'Wayland': have_wayland,
+    'Vulkan GUI': have_interface,
 }, section: 'Interface', bool_yn: true)
 
 summary({

commit ef2626c2f4b71f3324dff1e09dfd40132ad96233

Fix build with interface enabled
This fixes a build regression introduced by 669f466b41f50be9846c30476756a687ed3b9133.

src/lua_generic_api.c
@@ -16,6 +16,8 @@
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
  */
 
+#include "../config.h"
+
 #ifdef HAVE_INTERFACE
 #include "interface_common.h"
 #endif